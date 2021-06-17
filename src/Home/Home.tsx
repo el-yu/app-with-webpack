f618e4a9d0efba1d7649bf6c385254e3ef64ea2c
Elizabeth Yu <eyuu24@hotmail.com>
2021-06-17 22:38:37 +1000

    initial commit

---
+++ src/Home/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Home = () => {
+    return (<div style={{fontSize: '100px'}}>About ME</div>);
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'build'),
+        port: 3000,
+        historyApiFallback: true
+    },
+    entry: {
+        index: path.resolve(__dirname, 'src/index.tsx')
+    },
+    output: {
+        path: path.resolve(__dirname, 'build')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: require.resolve('ts-loader')
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+        })
+    ]
+}
